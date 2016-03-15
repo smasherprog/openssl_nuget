90b305ce1fd20db9ea8d1da85378054b938f8c65
smasherprog <smasherprog@gmail.com>
2016-03-15 05:59:20 -0700

    openssl library builds and publishes successully

--- cleanbuild.bat
+++ cleanbuild.bat
@@ -1,4 +1,5 @@
 call del %1.tar
+call del *.nupkg
 call rd /s /q %1
 call rd /s /q "openssl-src-win32-VS2013" 
 call rd /s /q "openssl-src-win64-VS2013" 
--- nugetpush.bat
+++ nugetpush.bat
@@ -1,4 +1,4 @@
 copy open_ssl.targets open_ssl\build\native\
 copy open_ssl.nuspec open_ssl\
 nuget pack open_ssl\open_ssl.nuspec
-for /r . %%g in (*.nupkg) do nuget push %%g -Timeout 1200
+for /r . %%g in (*.nupkg) do nuget push %%g -Timeout 2147483
